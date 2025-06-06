91e5ab70a5282dcc49313fc9414f4cab86ba68d6
Tarasov Vladislav <467667@niuitmo.ru>
2025-06-06 12:22:12 +0300

    initiaziled command fields

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import "context"
 
+
+// Command types for managing workerks
+type CommandType int
+
+type Command struct{
+	Type CommandType 
+	WorkerID int	
+}
+
 type WorkerPool struct {
+	jobQueue chan string	// Channel for jobs
+	commandChan chan Command // Channel for worker Commands
 	ctx		context.Context // Main ctx
 	cancel	context.CancelFunc //Cacnel func for main ctx
 }
 
+
 func NewWorkerPool(ctx context.Context) *WorkerPool {
 	poolctx, cancel := context.WithCancel(ctx)
 	return &WorkerPool{
+		jobQueue: make(chan string),
+		commandChan: make(chan Command),
 		ctx: poolctx,
 		cancel: cancel,
 	}
